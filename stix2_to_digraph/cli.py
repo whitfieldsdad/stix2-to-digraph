a43769714a1c4aabe02f070d3a053a97780f13f4
Tyler Fisher <tylerfisher@tylerfisher.ca>
2023-12-26 19:18:17 -0500

    Add ability to export map of node names, aliases, and external IDs to internal IDs

--- stix2_to_digraph/cli.py
+++ stix2_to_digraph/cli.py
@@ -1,6 +1,45 @@
+import json
+from typing import Iterable, Optional
 import click
+
+import stix2_to_digraph.converter as converter
 
 
 @click.group()
-def main():
-    pass
+@click.option("--indent", default=4)
+@click.pass_context
+def main(ctx: click.Context, indent: int):
+    ctx.obj = {"indent": indent}
+
+
+@main.command("alias-map")
+@click.argument("paths", nargs=-1, type=click.Path(exists=True))
+@click.option("--allow-deprecated/--no-deprecated", default=False, show_default=True)
+@click.option("--allow-revoked/--no-revoked", default=False, show_default=True)
+@click.option("--include-names/--no-names", default=True, show_default=True)
+@click.option("--lowercase/--no-lowercase", default=True, show_default=True)
+@click.pass_context
+def get_aliases(
+    ctx: click.Context,
+    paths: Iterable[str],
+    allow_deprecated: bool,
+    allow_revoked: bool,
+    include_names: bool,
+    lowercase: bool,
+):
+    """
+    Get node aliases.
+    """
+    if not paths:
+        raise click.UsageError("No data sources specified")
+
+    data_source = converter.get_data_source(paths)
+    rows = tuple(
+        converter.iter_stix2_objects(
+            data_source=data_source,
+            ignore_deprecated=allow_deprecated is False,
+            ignore_revoked=allow_revoked is False,
+        )
+    )
+    m = converter.get_alias_map(rows, include_names=include_names, lowercase=lowercase)
+    print(json.dumps(m, sort_keys=True, indent=ctx.obj["indent"]))
--- stix2_to_digraph/converter.py
+++ stix2_to_digraph/converter.py
@@ -1,5 +1,9 @@
+from concurrent.futures import ThreadPoolExecutor
+import copy
+import dataclasses
 import json
-from typing import Iterable, Optional, Union
+import sys
+from typing import Any, Dict, Iterable, Iterator, Optional, Union
 import networkx as nx
 from stix2.datastore import DataSource
 from dataclasses import dataclass
@@ -10,63 +14,144 @@ import requests
 from stix2 import (
     FileSystemSource,
     MemoryStore,
+    MemorySource,
     CompositeDataSource,
 )
-from stix2.datastore import DataSource
+from stix2.datastore import DataStoreMixin
 from stix2 import Filter
 from typing import Iterable, Union
+import networkx.readwrite.json_graph as json_graph
+from dataclasses import dataclass
+from stix2.serialization import STIXJSONEncoder as _JSONEncoder
+from stix2.base import _STIXBase
+from stix2.utils import STIXdatetime
+from urllib.parse import urlparse
 
-CREATED = "created"
-MODIFIED = "modified"
+
+UUID_NAMESPACE = "7ab40379-3225-406f-8872-a1c24bc229d2"
+
+CREATED_BY = "created-by"
+MODIFIED_BY = "modified-by"
 RELATED_TO = "related-to"
+APPLIES_TO = "applies-to"
+COMPONENT_OF = "component-of"
+
+KEYS_TO_DROP = {
+    "object_marking_refs",
+    "created_by_ref",
+    "x_mitre_version",
+    "revoked",
+    "x_mitre_attack_spec_version",
+    "x_mitre_modified_by_ref",
+    "x_mitre_deprecated",
+    "x_mitre_data_source_ref",
+}
 
 
-@dataclass()
-class Converter:
-    data_source: Union[str, MemoryStore, DataSource]
-    add_created_by_ref: bool = False
-    add_modified_by_ref: bool = False
+class JSONEncoder(_JSONEncoder):
+    def default(self, o: Any) -> Any:
+        if isinstance(o, nx.DiGraph):
+            return json_graph.node_link_data(o)
+        elif dataclasses.is_dataclass(o):
+            return dataclasses.asdict(o)
+        elif isinstance(o, _STIXBase):
+            return dict(o)
+        elif isinstance(o, STIXdatetime):
+            return o.isoformat()
+        elif iter(o) == o:
+            return list(o)
+        else:
+            return super().default(o)
 
-    def __post_init__(self):
-        self.data_source = get_data_source(self.data_source)
 
-    def stix2_to_networkx(
-        self, query: Optional[str] = None, compact: bool = True
-    ) -> nx.DiGraph:
-        """
-        Creates a nx.DiGraph from a list of STIX 2 objects.
+def iter_stix2_objects(
+    data_source: Union[str, MemoryStore, DataSource],
+    stix2_filters: Optional[Iterable[Union[str, Filter]]] = None,
+    ignore_deprecated: bool = True,
+    ignore_revoked: bool = True,
+) -> Iterator[dict]:
+    if stix2_filters:
+        stix2_filters = parse_stix2_filters(stix2_filters)
+    rows = data_source.query(stix2_filters)
+    rows = stix2_objects_to_dicts(
+        filter_stix2_objects(
+            rows,
+            ignore_deprecated=ignore_deprecated,
+            ignore_revoked=ignore_revoked,
+        )
+    )
+    yield from rows
 
-        - Nodes only contain IDs or URLs
-        - Edges only contain labels
-        """
-        g = nx.DiGraph()
-        objects = self.data_source.query(query)
-        for o in objects:
-            stix2_id = o["id"]
-            if compact:
-                g.add_node(stix2_id)
-            else:
-                g.add_node(stix2_id, **o)
 
-            if o["type"] == "relationship":
-                g.add_edge(
-                    o["source_ref"],
-                    o["target_ref"],
-                    relationship_type=o["relationship_type"],
-                )
-            else:
-                g.add_node(stix2_id)
+def filter_stix2_objects(
+    objects: Iterable[Any],
+    ignore_deprecated: bool = True,
+    ignore_revoked: bool = True,
+) -> Iterator[Any]:
+    if ignore_deprecated:
+        objects = filter(lambda o: o.get("x_mitre_deprecated") is not True, objects)
+        objects = filter(
+            lambda o: o.get("x_capec_status", "").lower() is not "deprecated", objects
+        )
 
-            if self.add_created_by_ref:
-                created_by = o.get("created_by_ref")
-                if created_by:
-                    g.add_edge(created_by, stix2_id, relationship_type=CREATED)
+    if ignore_revoked:
+        objects = filter(lambda o: o.get("revoked") is not True, objects)
 
-            if self.add_modified_by_ref:
-                modified_by = o.get("x_mitre_modified_by_ref")
-                if modified_by:
-                    g.add_edge(modified_by, stix2_id, relationship_type=MODIFIED)
-        return g
+    yield from objects
+
+
+def stix2_objects_to_networkx(
+    objects: Iterable[Any],
+    add_created_by_ref: bool = False,
+    add_modified_by_ref: bool = False,
+    add_markings: bool = False,
+    ignore_deprecated: bool = True,
+    ignore_revoked: bool = True,
+) -> nx.DiGraph:
+    """
+    Creates a nx.DiGraph from a list of STIX 2 objects.
+    """
+    g = nx.DiGraph()
+    objects = stix2_objects_to_dicts(
+        filter_stix2_objects(
+            objects,
+            ignore_deprecated=ignore_deprecated,
+            ignore_revoked=ignore_revoked,
+        )
+    )
+    for o in objects:
+        stix2_id = o["id"]
+        stix2_type = o["type"]
+
+        if stix2_type not in ["relationship", "external_reference"]:
+            g.add_node(stix2_id)
+
+        if stix2_type == "relationship":
+            g.add_edge(
+                o["source_ref"],
+                o["target_ref"],
+                relationship_type=o["relationship_type"],
+            )
+        elif stix2_type == "x-mitre-data-component":
+            data_source_id = o["x_mitre_data_source_ref"]
+            g.add_edge(data_source_id, stix2_id, relationship_type=COMPONENT_OF)
+
+        if add_created_by_ref:
+            created_by = o.get("created_by_ref")
+            if created_by:
+                g.add_edge(stix2_id, created_by, relationship_type=CREATED_BY)
+
+        if add_modified_by_ref:
+            modified_by = o.get("x_mitre_modified_by_ref")
+            if modified_by:
+                g.add_edge(stix2_id, modified_by, relationship_type=MODIFIED_BY)
+
+        if add_markings:
+            marking_refs = o.get("object_marking_refs")
+            if marking_refs:
+                for marking_ref in marking_refs:
+                    g.add_edge(marking_ref, stix2_id, relationship_type=APPLIES_TO)
+    return g
 
 
 def networkx_to_dot(g: nx.DiGraph) -> str:
@@ -94,26 +179,44 @@ def networkx_to_dot(g: nx.DiGraph) -> str:
     return "\n".join(lines)
 
 
-def get_data_sources(
-    data_sources: Iterable[Union[str, DataSource]]
-) -> Iterable[DataSource]:
-    return [get_data_source(src) for src in data_sources]
+def get_alias_map(
+    rows: Iterable[Union[dict, _STIXBase]],
+    include_names: bool = True,
+    lowercase: bool = True,
+) -> Dict[str, str]:
+    m = {}
+    for o in rows:
+        stix2_id = o["id"]
+
+        if include_names:
+            name = o.get("name")
+            if name is not None:
+                m[name] = stix2_id
+
+            for alias in o.get("x_mitre_aliases", []):
+                m[alias] = stix2_id
+
+        for external_reference in o.get("external_references", []):
+            source_name = external_reference.get("source_name")
+            if source_name is not None and source_name in ["capec", "mitre-attack"]:
+                external_id = external_reference["external_id"]
+                m[external_id] = stix2_id
+
+    if lowercase:
+        m = {k.lower(): v for k, v in m.items()}
+    return m
 
 
-def get_composite_data_source(data_sources: Iterable[Union[str, DataSource]]):
-    composite = CompositeDataSource()
-    for data_source in data_sources:
-        composite.add_data_source(get_data_source(data_source))
-    return composite
-
-
-def get_data_source(data_source: Union[str, DataSource]) -> DataSource:
-    if isinstance(data_source, (DataSource, MemoryStore)):
-        return data_source
-    elif isinstance(data_source, str):
-        return _get_data_source(data_source)
+def get_data_source(
+    data_sources: Union[str, DataSource, Iterable[Union[str, DataSource]]]
+) -> Union[DataSource, CompositeDataSource]:
+    if isinstance(data_sources, (str, DataSource)):
+        return _get_data_source(data_sources)
     else:
-        raise ValueError(f"Invalid data source: {data_source}")
+        data_sources = [_get_data_source(ds) for ds in data_sources]
+        composite_data_source = CompositeDataSource()
+        composite_data_source.add_data_sources(data_sources)
+        return composite_data_source
 
 
 def _get_data_source(path: str) -> DataSource:
@@ -121,23 +224,23 @@ def _get_data_source(path: str) -> DataSource:
         if os.path.isdir(path):
             return FileSystemSource(path)
         else:
-            return _get_memory_store_from_file(path)
+            return _get_memory_source_from_file(path)
     elif path.startswith(("http://", "https://")):
-        return _get_memory_store_from_web(path)
+        return _get_memory_source_from_web(path)
     else:
         raise ValueError(f"Invalid path: {path}")
 
 
-def _get_memory_store_from_file(path: str) -> MemoryStore:
-    with open(path, "r") as file:
+def _get_memory_source_from_file(path: str) -> MemoryStore:
+    with open(path, "rb") as file:
         stix_data = json.load(file)
-        return MemoryStore(stix_data=stix_data)
+        return MemorySource(stix_data=stix_data)
 
 
-def _get_memory_store_from_web(url: str) -> MemoryStore:
+def _get_memory_source_from_web(url: str) -> MemoryStore:
     response = requests.get(url)
     response.raise_for_status()
-    return MemoryStore(response.json()["objects"])
+    return MemorySource(response.json()["objects"])
 
 
 def parse_stix2_filters(stix2_filters: Iterable[Union[str, Filter]]) -> List[Filter]:
@@ -156,6 +259,10 @@ def parse_stix2_filter(stix2_filter: str) -> Filter:
     - '>='
     - '<='
     - 'contains'
+
+    Examples:
+
+    - 'type = relationship'
     """
     m = re.match(r"(.+) (.+) (.+)", stix2_filter)
     if m:
@@ -167,3 +274,45 @@ def realpath(path: str) -> str:
     path = os.path.expanduser(path)
     path = os.path.realpath(path)
     return path
+
+
+def stix2_objects_to_dicts(rows: Iterable[Any]) -> Iterator[dict]:
+    with ThreadPoolExecutor() as executor:
+        futures = []
+        for row in rows:
+            if isinstance(row, dict):
+                yield row
+            else:
+                future = executor.submit(stix2_objects_to_dict, row)
+                futures.append(future)
+        yield from (future.result() for future in futures)
+
+
+def stix2_objects_to_dict(o: Any) -> dict:
+    if isinstance(o, _STIXBase):
+        b = json.dumps(o, cls=JSONEncoder)
+        o = json.loads(b)
+    elif isinstance(o, dict):
+        pass
+    else:
+        raise ValueError(f"Unsupported object type: {type(o).__name__}")
+    return o
+
+
+@dataclass()
+class DiGraphSummary:
+    total_nodes: int
+    total_edges: int
+
+    def to_dict(self) -> dict:
+        return dataclasses.asdict(self)
+
+    def to_json(self) -> str:
+        return json.dumps(self.to_dict())
+
+
+def summarize_digraph(g: nx.DiGraph) -> DiGraphSummary:
+    return DiGraphSummary(
+        total_nodes=g.number_of_nodes(),
+        total_edges=g.number_of_edges(),
+    )
